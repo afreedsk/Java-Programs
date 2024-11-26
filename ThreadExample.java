b2ac90823c6db8a775b2b8b10800f21f3f8d9e17
Shaik Afreed <165375955+afreedsk@users.noreply.github.com>
2024-11-26 12:37:17 -0800

    Add files via upload

---
+++ ThreadExample.java
@@ -0,0 +1,20 @@
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
