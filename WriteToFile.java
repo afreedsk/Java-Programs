41658f2aedb5b408066cf3837e08c9cd0ce5a9fb
Shaik Afreed <165375955+afreedsk@users.noreply.github.com>
2024-11-26 12:33:25 -0800

    Add files via upload

---
+++ WriteToFile.java
@@ -0,0 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("example.txt");
+            writer.write("Hello, this is a file handling example in Java!");
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
