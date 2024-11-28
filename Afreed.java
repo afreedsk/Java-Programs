7226c022d9ff73289b9df7d5ac53d05862206e03
Shaik Afreed <165375955+afreedsk@users.noreply.github.com>
2024-11-28 10:36:05 -0800

    Add files via upload

--- Afreed.java
+++ Afreed.java
@@ -1,56 +1,114 @@
+import java.util.Scanner;
 
-class School {
-    public static void sriPoojitha() {
-        int schoolage = 15;
-        System.out.println("I am in 10th Class");
-        System.out.println("School Age : " + schoolage);
-    }
+class Family {
+    public void famliy() {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to My family");
+        System.out.println("We have 4 members");
+        System.out.println("1. Father");
+        System.out.println("2. Mother");
+        System.out.println("3. Brother");
+        System.out.println("4. Me");
+        System.out.print("Who's Information you needed  : ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("You have Selected Father");
+                System.out.println("Father's Name : " + "SM. Basha");
+                System.out.println("Father's Age : " + "  45");
+                System.out.println("Father's Occupation : " + "Real State");
+                break;
 
-    public double sriPoojitha(double percentage) {
-        // Use the parameter directly instead of redefining it.
-        System.out.println("10th Class Percentage : " + percentage);
-        return percentage;
+            case 2:
+                System.out.println("You have Selected Mother");
+                System.out.println("Mother's Name : " + "Shaik. Faizun");
+                System.out.println("Mother's Age : " + "  40");
+                System.out.println("Mother's Occupation : " + "Asha Worker");
+                break;
+
+            case 3:
+                System.out.println("You have Selected Brother");
+                System.out.println("Mother's Name : " + "Shaik.Afzal");
+                System.out.println("Mother's Age : " + "  20");
+                System.out.println("Brother is Studying : " + "BIPC");
+                break;
+
+            case 4:
+                System.out.println("You have Selected Me");
+                System.out.println("My Name : " + "Shaik Afreed");
+                System.out.println("My Age : " + "  22");
+                System.out.println("My Occupation : " + "Bewarse");
+                break;
+
+            default:
+                System.out.println("Invalid Choice");
+
+        }
     }
 }
 
-class Diploma extends School {
-    public static void mbtsClg() {
-        int diplomaage = 18;
-        System.out.println("I am in Diploma");
-        System.out.println("Diploma Age : " + diplomaage);
+class MyEducation {
+    public void MySchool() {
+        System.out.println("From Nuresy to 6th Class I am in Srestha Model School");
+        System.out.println("From 7th Class to 10th Class I am in Sri Poojitha Public School");
+        System.out.println("I got 10GPA");
     }
 
-    public double diploma(double percentage) {
-        System.out.println("Diploma Percentage : " + percentage);
-        return percentage;
+    public void MyDiploma() {
+        System.out.println("In Diploma I am Electrical Electronics and Engineering");
+        System.out.println("I got 7.2GPA");
+        System.out.println("I Enjoyed Alot");
+    }
+
+    public void MyBtech() {
+        System.out.println("In Btech I am Computer Science and Engineering");
+        System.out.println("I got 8.0GPA");
+        System.out.println("I Enjoyed Alot");
     }
 }
 
-class BTech extends Diploma {
-    public static void BtechClg() {
-        int btechage = 21;
-        System.out.println("I am in B.Tech");
-        System.out.println("B.Tech Age : " + btechage);
+class MySkills {
+    public void mySkills() {
+        System.out.println("My Skills are :");
+        System.out.println("1. HTML");
+        System.out.println("2. CSS");
+        System.out.println("3. JavaScript");
+        System.out.println("4. React.js");
+        System.out.println("5. Java");
+        System.out.println("6. SpringBoot");
+        System.out.println("7. JDBC");
+        System.out.println("8. MySQL");
+        System.out.println("9. MongoDB");
+        System.out.println("10. RestAPI's");
+        System.out.println("11. Git, GitHub");
+        System.out.println("I am a Full Stack Developer");
     }
+}
 
-    public double btech(double percentage) {
-        System.out.println("B.Tech Percentage : " + percentage);
-        return percentage;
+class MyCareer {
+    public void myCareer() {
+        System.out.println("My Career Goal is to be a Full Stack Developer");
+        System.out.println("I want to work in a MNC Company");
+        System.out.println("I want to earn a good salary");
     }
 }
 
 class Afreed {
     public static void main(String[] args) {
-        School s = new School();
-        School.sriPoojitha(); // Call static method using the class name
-        s.sriPoojitha(100);
-
-        Diploma d = new Diploma();
-        Diploma.mbtsClg(); // Call static method using the class name
-        d.diploma(72.3);
-
-        BTech b = new BTech();
-        BTech.BtechClg(); // Call static method using the class name
-        b.btech(78.9);
+        Scanner sc = new Scanner(System.in);
+        Family f = new Family();
+        f.famliy();
+        System.out.println("-------------------------------------------");
+        MyEducation e = new MyEducation();
+        e.MySchool();
+        e.MyDiploma();
+        e.MyBtech();
+		System.out.println("--------------------------------------------");
+        MySkills s = new MySkills();
+        s.mySkills();
+		System.out.println("--------------------------------------------");
+        MyCareer c = new MyCareer();
+        c.myCareer();
+		System.out.println("Thank You Buddy");
     }
 }
