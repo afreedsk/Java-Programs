9a68dd9a0ed539a23d831798f571a3109f3e8656
Shaik Afreed <165375955+afreedsk@users.noreply.github.com>
2024-11-28 11:12:03 -0800

    Add files via upload

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,50 +1,39 @@
-import java.util.*;
-class BinarySearch
-{
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the Array Size : ");
-		int arr[] = new int[sc.nextInt()];
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.print("Enter the Element : "+(i+1)+" : ");
-			arr[i] = sc.nextInt();
-		}
-		System.out.print("Enter the Your Element to find : ");
-		int key = sc.nextInt();
-		if(isBinarySearch(arr,key))
-		{
-			System.out.print("Is found");
-		}
-		else
-		{
-			System.out.print("Is Not found");
-		}
-	}
-	public static boolean isBinarySearch(int[] arr,int key)
-	{
-		
-		int start = 0;
-		int end = arr.length-1;
-		int mid = 0;
-		while(start<=end)
-		{
-			mid = (start+end)/2;
-			if(arr[mid]==key)
-			{
-				 return true;
-			}
-			else if(arr[mid]<key)
-			{
-				start = mid+1;
-			}
-			else
-			{
-				end = mid-1;
-			}
-		}
-		return false;
-	}
-	
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the sorted elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the element to search: ");
+        int target = sc.nextInt();
+
+        int result = binarySearch(arr, target, 0, n - 1);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
